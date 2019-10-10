2842ff2225675c163bdfc1a99cd951ec8f9f34c9
Euclidr <euclidry@gmail.com>
2019-10-10 17:03:27 +0800

    add apidoc parser

--- definition-page/apidoc.py
+++ definition-page/apidoc.py
@@ -1,27 +1,171 @@
 # -*- coding:utf-8 -*-
 
-from pathlib import Path
-from typing import List
+# https://stackoverflow.com/questions/15853469/putting-current-class-as-return-type-annotation
+from __future__ import annotations
 
-## #/definitions/
+import json
+import logging
+from collections import OrderedDict
+from pathlib import Path
+from typing import List, Dict, Optional
+from resource import Resources
+
+logger = logging.getLogger("data.apidoc")
+
+class ApiDoc(object):
+
+    def __init__(self, version):
+        self._storage = {}
+        self._version = version
+        self._resouces = Resources(version)
+        self._load_doc(version)
+
+    @property
+    def storage(self) -> Dict[str, DefItem]:
+        return self._storage
+
+    @property
+    def resources(self) -> Resources:
+        return self._resouces
+
+    def fetch(self, key) -> Optional[DefItem]:
+        return self.storage.get(key)
+
+    def store(self, key, value: DefItem):
+        self.storage[key] = value
+
+    def search(self, path: str) -> Optional[DefItem]:
+        root = path.split('.')[0]
+        root_key = self.find_root_key(root)
+        if not root_key:
+            return None
+        print('root_key of {} is {}'.format(path, root_key))
+
+        root_item = self.fetch(root_key)
+
+        parts = path.split('.', maxsplit=1)
+        if len(parts) == 1:
+            return root_item
+
+        return root_item.search(parts[1])
+
+    def find_root_key(self, root: str) -> Optional[str]:
+        res = self.resources.get(root)
+        if not res:
+            return None
+
+        released_versions = ['v5', 'v4', 'v3', 'v2', 'v1']
+        suffix = '.' + res.kind
+        candidates = [k for k in self.storage.keys() if k.endswith(suffix)]
+        candidates.sort(key=lambda x: x.split('.')[-2], reverse=True)
+        for v in released_versions:
+            for candidate in candidates:
+                if candidate.split('.')[-2] == v:
+                    return candidate
+        return candidates[0]
+
+    def _load_doc(self, version: str):
+        path = Path('data/{}/swagger.json'.format(version))
+        if not path.exists():
+            raise FileNotFoundError()
+
+        with path.open() as f:
+            doc_dict = json.load(f, object_pairs_hook=OrderedDict)
+            definitions = doc_dict['definitions']
+            for (key, detail) in definitions.items():
+                item = DefItem.from_dict(self, detail)
+                self.store(key, item)
+
 
 class DefItem(object):
 
     def __init__(self):
         self.lookup = None
+        self._type = ''
+        self._ref_link = ''
+        self._properties = OrderedDict()
+
+    def search(self, path: str) -> Optional[DefItem]:
+        if not path:
+            return self
+
+        if self.type != 'object':
+            return None
+
+        root = path.split('.')[0]
+        item = self.properties.get(root)
+        if not item:
+            return None
+
+        parts = path.split('.', maxsplit=1)
+        if len(parts) == 1:
+            return item
+
+        return item.search(parts[1])
+
+    @classmethod
+    def from_dict(cls, lookup: ApiDoc, raw: Dict[str, object]) -> Optional[DefItem]:
+        item = DefItem()
+        item.lookup = lookup
+        item._description = raw.get('description', '')
+
+        item._type = raw.get('type', '')
+        if item._type in ['boolean', 'string']:
+            return item
+
+        if item._type in ['number', 'integer']:
+            item._format = raw.get('format', '')
+            return item
+
+        if item._type == 'array':
+            inner_item = raw.get('items', {})
+            item._array_item = cls.from_dict(lookup, inner_item)
+            return item
+
+        if item._type != '' and item._type != 'object':
+            print('Unknown item type: {}'.format(item._type))
+            return None
+
+        item._required = raw.get('required', [])
+
+        ref_link = raw.get('$ref', '')
+        if ref_link.startswith('#/definitions/'):
+            ref_link = ref_link[len('#/definitions/'):]
+        item._ref_link = ref_link
+        if ref_link:
+            return item
+
+        raw_properties = raw.get('properties', OrderedDict())
+        item._type = 'object'
+        properties = OrderedDict()
+        for (key, detail) in raw_properties.items():
+            dict_item = cls.from_dict(lookup, detail)
+            if dict_item:
+                properties[key] = dict_item
+        # if mark:
+        #     print(properties)
+        item._properties = properties
+
+        # TODO deal with additionalProperties
+
+        return item
+
+    @property
+    def def_id(self) -> str:
+        return self._def_id
+
+    @def_id.setter
+    def def_id(self, value: str):
+        self._def_id = value
 
     @property
     def ref_link(self) -> str:
         return self._ref_link
 
-    @ref_link.setter
-    def ref_link(self, value: str):
-        self._ref_link = value
-
     @property
     def ref(self) -> DefItem:
         if self.ref_link and self.lookup:
-            return self.lookup.get(self.ref_link)
+            return self.lookup.fetch(self.ref_link)
         return None
 
     @property
@@ -30,11 +174,7 @@ class DefItem(object):
             return self._description
         if self.ref:
             return self.ref.description
-        return None
-
-    @description.setter
-    def description(self, value: str):
-        self._description = value
+        return ''
 
     @property
     def type(self) -> str:
@@ -42,30 +182,30 @@ class DefItem(object):
             return self._type
         if self.ref:
             return self.ref.type
-        return None
+        return ''
 
-    @type.setter
-    def type(self, value: str):
-        self._type = value
+    @property
+    def format(self) -> str:
+        if self.type in ['number', 'integer']:
+            return self._format
+        return ''
 
     @property
     def properties(self):
-        return self._properties
+        if self._properties:
+            return self._properties
 
-    @properties.setter
-    def properties(self, value):
-        self._properties = value
+        if self.ref:
+            return self.ref.properties
+        
+        return self._properties
 
     @property
     def required(self) -> List[str]:
         return self._required
 
-    @required.setter
-    def required(self, value: List[str]):
-        self._required = value
-
     @property
-    def array_item(self) -> DefItem:
+    def array_item(self):
         if not self.type == 'array':
             return None
         if self._array_item:
@@ -74,10 +214,32 @@ class DefItem(object):
             return self.ref.array_item
         return None
 
-    @array_item.setter
-    def array_item(self, value: DefItem):
-        self._array_item = value
+    def to_dict(self):
+        result = OrderedDict()
+        result['description'] = self.description
+        result['type'] = self.type
+        if self.type in ['number', 'integer']:
+            result['format'] = self.format
+        if self.type == 'array':
+            result['items'] = self.array_item.to_dict()
+        if self.type == 'object':
+            result['properties'] = OrderedDict()
+            for (key, item) in self.properties.items():
+                result['properties'][key] = item.to_dict()
+            result['required'] = self.required
+        return result
 
+
+if __name__ == '__main__':
+    doc = ApiDoc('1.13.7')
+    rqs = doc.fetch('io.k8s.api.core.v1.ResourceQuotaStatus')
+    item = doc.search('po.metadata')
+    print(item.description)
+    # print(item.ref_link)
+    # print(item.ref)
+    # print(item.ref.properties.keys())
+    print(item.properties.keys())
+    # print(json.dumps(rqs.to_dict(), indent=4))
 
 # definitions
 #     description
--- definition-page/resource.py
+++ definition-page/resource.py
@@ -1,9 +1,108 @@
 #! -*- coding:utf-8 -*-
+from __future__ import annotations
+
 from pathlib import Path
-from typing import List
+from typing import List, Optional
 
 
-class Resource:
+class Resources(object):
+
+    def __init__(self, version: str):
+        self.name_mapper = {}
+        self.kind_mapper = {}
+        self.short_mapper = {}
+        self._load_resources(version)
+
+    def _load_resources(self, version: str):
+        path = Path('data/{}/api-resources.txt'.format(version))
+        if not path.exists():
+            raise FileNotFoundError()
+
+        with path.open() as f:
+            head = f.readline()
+            col_widths = self._extract_col_widths(head)
+            print(col_widths)
+            for l in f:
+                resource = self._extract_resource(l, col_widths)
+                if resource:
+                    self.add(resource)
+
+        return self
+
+    def add(self, res: Resource):
+        self.name_mapper[res.name] = res
+        self.kind_mapper[res.kind] = res
+        if res.short_names:
+            for short_name in res.short_names.split(','):
+                self.short_mapper[short_name] = res
+
+    def get_by_name(self, name: str) -> Optional[Resource]:
+        return self.name_mapper.get(name)
+
+    def get_by_short(self, short: str) -> Optional[Resource]:
+        return self.short_mapper.get(short)
+
+    def get_by_kind(self, kind: str) -> Optional[Resource]:
+        return self.kind_mapper.get(kind)
+
+    def get(self, key: str) -> Optional[Resource]:
+        r = self.get_by_name(key)
+        if r:
+            return r
+        r = self.get_by_kind(key)
+        if r:
+            return r
+        return self.get_by_short(key)
+
+    @staticmethod
+    def _extract_resource(l: str, col_widths: List[int]) -> Resource:
+        min_col_width = sum(col_widths)
+        if len(l) < min_col_width:
+            return None
+
+        NAME, SHORTNAME, APIGROUP, NAMESPACED = 0, 1, 2, 3
+
+        name = l[:col_widths[NAME]].strip()
+        l = l[col_widths[NAME]:]
+
+        short_names = l[:col_widths[SHORTNAME]].strip()
+        l = l[col_widths[SHORTNAME]:]
+
+        api_group = l[:col_widths[APIGROUP]].strip()
+        l = l[col_widths[APIGROUP]:]
+
+        namespaced = l[:col_widths[NAMESPACED]].strip()
+        namespaced = True if namespaced == 'true' else False
+        l = l[col_widths[NAMESPACED]:]
+
+        kind = l.strip()
+        if not kind:
+            return None
+
+        r = Resource()
+        r.name = name
+        r.short_names = short_names
+        r.api_group = api_group
+        r.namespaced = namespaced
+        r.kind = kind
+        return r
+
+
+    @staticmethod
+    def _extract_col_widths(head: str) -> List[int]:
+        '''get column width except for the last column'''
+        pre_blank, pre_idx = False, 0
+        result = []
+        for i, c in enumerate(head):
+            if pre_blank and c != ' ':
+                result.append(i-pre_idx)
+                pre_idx = i
+            pre_blank = c == ' '
+        return result
+
+
+
+class Resource(object):
 
     @property
     def name(self) -> str:
@@ -14,12 +113,12 @@ class Resource:
         self._name = value
 
     @property
-    def short_name(self) -> str:
-        return self._short_name
+    def short_names(self) -> str:
+        return self._short_names
 
-    @short_name.setter
-    def short_name(self, value: str):
-        self._short_name = value
+    @short_names.setter
+    def short_names(self, value: str):
+        self._short_names = value
 
     @property
     def api_group(self) -> str:
@@ -46,68 +145,11 @@ class Resource:
         self._kind = value
 
     def __repr__(self):
-        return "Resource(name: {}, kind: {} short_name: {}, api_group: {}, namespaced: {})".format(
-            self.name, self.kind, self.short_name, self.api_group, self.namespaced
+        return "Resource(name: {}, kind: {} short_names: {}, api_group: {}, namespaced: {})".format(
+            self.name, self.kind, self.short_names, self.api_group, self.namespaced
         )
 
 
-def load_resource(version: str):
-    path = Path('data/{}/api-resources.txt'.format(version))
-    if not path.exists():
-        raise FileNotFoundError()
-    with path.open() as f:
-        head = f.readline()
-        col_widths = _extract_col_widths(head)
-        print(col_widths)
-        for l in f:
-            resource = _extract_resource(l, col_widths)
-            print(resource)
-
-
-def _extract_resource(l: str, col_widths: List[int]) -> Resource:
-    min_col_width = sum(col_widths)
-    if len(l) < min_col_width:
-        return None
-
-    NAME, SHORTNAME, APIGROUP, NAMESPACED = 0, 1, 2, 3
-
-    name = l[:col_widths[NAME]].strip()
-    l = l[col_widths[NAME]:]
-
-    short_name = l[:col_widths[SHORTNAME]].strip()
-    l = l[col_widths[SHORTNAME]:]
-
-    api_group = l[:col_widths[APIGROUP]].strip()
-    l = l[col_widths[APIGROUP]:]
-
-    namespaced = l[:col_widths[NAMESPACED]].strip()
-    namespaced = True if namespaced == 'true' else False
-    l = l[col_widths[NAMESPACED]:]
-
-    kind = l.strip()
-    if not kind:
-        return None
-
-    r = Resource()
-    r.name = name
-    r.short_name = short_name
-    r.api_group = api_group
-    r.namespaced = namespaced
-    r.kind = kind
-    return r
-
-
-def _extract_col_widths(head: str) -> List[int]:
-    '''get column width except for the last column'''
-    pre_blank, pre_idx = False, 0
-    result = []
-    for i, c in enumerate(head):
-        if pre_blank and c != ' ':
-            result.append(i-pre_idx)
-            pre_idx = i
-        pre_blank = c == ' '
-    return result
-
 
 if __name__ == '__main__':
-    load_resource('1.13.7')
+    Resources('1.13.7')
